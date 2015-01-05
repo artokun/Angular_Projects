20db46f1fd67b7f6bff93701eb841622a5ec52bc
artokun <klutter1496@yahoo.com>
2015-01-04 18:41:59 -0800

    Changed service value to factory in services.js. will keep both for learning purposes. factory lets you pass a function

--- appengine-angular-seed-python-master/app/js/services.js
+++ appengine-angular-seed-python-master/app/js/services.js
@@ -6,4 +6,7 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
-    .value('FIREBASE_URL', 'https://waitandeat-art.firebaseio.com/');
+  //.value('FIREBASE_URL', 'https://waitandeat-art.firebaseio.com/');
+  .factory('FIREBASE_URL', function() {
+  return 'https://waitandeat-art.firebaseio.com/';
+});
