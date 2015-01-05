7f32974f9a487fd655d180840e3f5ac3894fcd54
artokun <klutter1496@yahoo.com>
2015-01-04 18:10:06 -0800

    Debugging GIT
    Did a branch > master merge. trying to debug

--- appengine-angular-seed-python-master/app/js/controllers.js
+++ appengine-angular-seed-python-master/app/js/controllers.js
@@ -28,8 +28,37 @@ angular.module('myApp.controllers', [])
         name: party.name
       };
       textMessages.$add(newTextMessasge);
-      // Code here for notified
-      party.notified = 'Yes';
+      party.notified = 'Yes'; // Code here for notified
       $scope.parties.$save(party.$id);
+    };
+  }])
+  .controller('AuthController', ['$scope', '$firebaseSimpleLogin', '$location', function($scope, $firebaseSimpleLogin, $location) {
+    var authRef = new Firebase('http://waitandeat-art.firebaseio.com/');
+
+    var auth = $firebaseSimpleLogin(authRef);
+
+    $scope.user = {email:'', password:''};
+
+    $scope.register = function() {
+      auth.$createUser($scope.user.email, $scope.user.password).then(function(data) {
+        console.log(data);
+        $scope.login();
+        //TODO: show error when registration fails
+        //TODO: make password criteria with jQuery
+      });//.then(functionSuccess(data), functionFailure(data))
+    };
+
+    $scope.login = function() {
+      auth.$login('password', $scope.user).then(function(data) {
+        console.log(data);
+        //Redirect users to the waitlist page /waitlist
+        $location.path('/waitlist');
+      });
+    };
+
+    $scope.logout = function() {
+      auth.$logout();
+      //Redirect users to the landing page /
+      $location.path('/');
     };
   }]);
