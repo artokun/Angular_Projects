7b8f44dda46c2b9f3ab777b03295b7c74b8be14b
artokun <klutter1496@yahoo.com>
2015-01-04 20:11:58 -0800

    Added functionality to authService to toggle visibility on the login and logout links depending on whether there is a user logged in or not.

--- appengine-angular-seed-python-master/app/js/services.js
+++ appengine-angular-seed-python-master/app/js/services.js
@@ -7,7 +7,7 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
   .value('FIREBASE_URL', 'https://waitandeat-art.firebaseio.com/')
-  .factory('authService', function($firebaseSimpleLogin, $location, FIREBASE_URL) {
+  .factory('authService', function($firebaseSimpleLogin, $location, FIREBASE_URL, $rootScope) {
     var authRef = new Firebase(FIREBASE_URL);
     var auth = $firebaseSimpleLogin(authRef);
 
@@ -29,5 +29,17 @@ angular.module('myApp.services', [])
         $location.path('/');
       }
     };
-      return authServiceObject;
+
+    $rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
+      console.log("User " + user.id + "successfully logged in!");
+      //Save currentUser on our rootScope.
+      $rootScope.currentUser = user;
+    });
+    $rootScope.$on("$firebaseSimpleLogin:logout", function() {
+      console.log("User has successfully logged out!");
+      //Save currentUser on our rootScope.
+      $rootScope.currentUser = null;
+    });
+
+    return authServiceObject;
 });
